6e104df73647a25fb5c053567a7516a61ab1f082
Jeffrey Kirchner <jkirchner@gmail.com>
2025-03-17 16:30:04 +0000

    Add group selection to summary card and update treatment handling methods. fixes #42.

--- main/templates/staff/staff_session/summary/summary_card.js
+++ main/templates/staff/staff_session/summary/summary_card.js
@@ -1,4 +1,15 @@
+/**
+ * maually change the current treatment during setup
+ */
 update_current_treatment: function update_current_treatment() 
+{
+    app.do_reload();
+},
+
+/**
+ * change the current group during a session
+ */
+update_current_group: function update_current_group() 
 {
     app.do_reload();
 },
